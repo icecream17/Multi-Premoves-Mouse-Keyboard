0dcfb0c56a2004fbc21dad858c09d789f2ef6b82
Sentero-esp12 <35060206+Sentero-esp12@users.noreply.github.com>
2020-10-14 18:21:26 +0300

    update.js

--- Extension/chrome.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --silent-debugger-extension-api
---
+++ Extension/update.js
@@ -0,0 +1,8 @@
+let updateObject = {
+   versions: {
+      content: 1,
+      script: 1,
+      background: 1
+   },
+   additionalSettings: {}
+}
